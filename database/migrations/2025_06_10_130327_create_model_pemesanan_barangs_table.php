56aea70ae1df00b2308435cbff32971ba6b24068
armadi <armadiam18@gmail.com>
2025-06-10 23:00:01 +0800

    first-commit

---
+++ database/migrations/2025_06_10_130327_create_model_pemesanan_barangs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('model_pemesanan_barangs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userid');
+            $table->string('kode_transaksi');
+            $table->string('nama_pengirim');
+            $table->string('nomor_hp');
+            $table->string('alamat_pengiriman');
+            $table->string('foto_produk');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('userid')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('model_pemesanan_barangs');
+    }
+};

a46bb027dc547e5e3d5643ff5be1562f7adf2296
armadi <armadiam18@gmail.com>
2025-06-11 02:28:24 +0800

    Add PemesananBarangController.php

---
+++ app/Http/Resources/BarangResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BarangResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'kode_transaksi' => $this->kode_transaksi,
+            'nama_pengirim' => $this->nama_pengirim,
+            'nomor_hp' => $this->nomor_hp,
+
+        ];
+    }
+}
---
+++ app/Http/Resources/SemuaBarangResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SemuaBarangResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'kode_transaksi' => $this->kode_transaksi,
+            'nama_pengirim' => $this->nama_pengirim,
+            'nomor_hp' => $this->nomor_hp,
+            'alamat_pengiriman' => $this->alamat_pengiriman,
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
